b68ff1d6148744f5ef7ca69b70e71de2ca3711c1
hedudelgado <hedu.delgado@gmail.com>
2016-02-01 23:51:41 +0000

    First commit, chapter 8 finished

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do 
+	'hellooOoO!'
+end
+
+
+get '/secreto' do
+  'This is a secret page'
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
